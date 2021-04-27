ad6d80c81959177765defb277e217afd4d833ae2
Nathaniel Derosier <nathaniel.derosier@gmail.com>
2021-04-27 01:15:57 -0500

    Added player input and validity check

--- RPS-js.js
+++ RPS-js.js
@@ -1,5 +1,16 @@
+playerThrow = window.prompt('Rock, Paper, Scissors. Shoot!');
+
+if (playerThrow.toLowerCase() === 'rock' || playerThrow.toLowerCase() == 'paper' || playerThrow.toLowerCase() == 'scissors') {
+    console.log(playerThrow);
+} else {
+    console.log('Invalid Move');
+}
+
 function computerPlay(throws) {
     return throws[Math.floor(Math.random()*throws.length)];
 }
+
 const throws = ['Rock', 'Paper', 'Scissors'];
+
+
 console.log(computerPlay(throws));
